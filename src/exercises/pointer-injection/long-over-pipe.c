e6f712c71a0ebb25aff9d28b22b37c588f981601
Brooks Davis <brooks@one-eyed-alien.net>
2020-08-31 14:12:39 -0700

    Fill out the pointer-injection exercise.

---
+++ src/exercises/pointer-injection/long-over-pipe.c
@@ -0,0 +1,27 @@
+#include <err.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	int fds[2];
+	pid_t pid;
+	long val;
+
+	if (pipe(fds) == -1)
+		err(1, "pipe");
+	if ((pid = fork()) == -1)
+		err(1, "fork");
+	if (pid == 0) {
+		val = 42;
+		if (write(fds[0], &val, sizeof(val)) != sizeof(val))
+			err(1, "write");
+	} else {
+		if (read(fds[1], &val, sizeof(val)) != sizeof(val))
+			err(1, "read");
+		printf("received %ld\n", val);
+	}
+
+	return 0;
+}
---
+++ src/exercises/pointer-injection/ptr-over-pipe.c
@@ -0,0 +1,29 @@
+#include <err.h>
+#include <stdio.h>
+#include <unistd.h>
+
+const char hello[] = "Hello world!";
+
+int
+main(void)
+{
+	int fds[2];
+	pid_t pid;
+	const char *ptr;
+
+	if (pipe(fds) == -1)
+		err(1, "pipe");
+	if ((pid = fork()) == -1)
+		err(1, "fork");
+	if (pid == 0) {
+		ptr = hello;
+		if (write(fds[0], &ptr, sizeof(ptr)) != sizeof(ptr))
+			err(1, "write");
+	} else {
+		if (read(fds[1], &ptr, sizeof(ptr)) != sizeof(ptr))
+			err(1, "read");
+		printf("received %s\n", ptr);
+	}
+
+	return 0;
+}
