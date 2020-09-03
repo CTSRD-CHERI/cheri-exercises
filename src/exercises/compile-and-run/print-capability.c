3192ddc6fbeb8dfce3463f4a30201fd397db2f0d
Brooks Davis <brooks@one-eyed-alien.net>
2020-09-03 08:35:03 -0700

    Switch to block comments for headers.

--- src/exercises/compile-and-run/print-capability.c
+++ src/exercises/compile-and-run/print-capability.c
@@ -1,5 +1,7 @@
-// SPDX-License-Identifier: BSD-2-Clause-DARPA-SSITH-ECATS-HR0011-18-C-0016
-// Copyright 2020 SRI International
+/*
+ * SPDX-License-Identifier: BSD-2-Clause-DARPA-SSITH-ECATS-HR0011-18-C-0016
+ * Copyright 2020 SRI International
+ */
 #include <stdio.h>
 #include <cheriintrin.h>
 
