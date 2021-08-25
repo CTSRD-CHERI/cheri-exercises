94479ba4fb4ebdeef80b76cdfd4d69183fa15882
nwf <nwf@users.noreply.github.com>
2021-08-25 17:23:30 +0000

    deploy: 07138da688d0e01b59920c4c3f2b8ea8222f675d

--- missions/use-after-free-control-flow/temporal-mission.c
+++ missions/use-after-free-control-flow/temporal-mission.c
@@ -12,8 +12,8 @@
 #include <sys/queue.h>
 
 #if defined(__CHERI_PURE_CAPABILITY__)
-#include <sys/caprevoke.h>
-static void __attribute__((used)) *check_caprevoke = caprevoke;
+#include <cheri/revoke.h>
+static void __attribute__((used)) *check_cheri_revoke = cheri_revoke;
 extern void malloc_revoke(void);
 static void __attribute__((used)) *check_malloc_revoke = malloc_revoke;
 #endif
@@ -66,8 +66,8 @@ descr_2(struct crop *c)
 static unsigned int cid;
 static union ufo *ufo;
 
-static const size_t nfarm = 4;
-struct farm *farmp[nfarm];
+#define NFARM 4
+struct farm *farmp[NFARM];
 
 static void
 rm_farm(int fix)
@@ -167,7 +167,7 @@ main(void)
 		if (isspace(c))
 			continue;
 
-		if (('0' <= c) && (c < '0' + nfarm)) {
+		if (('0' <= c) && (c < '0' + NFARM)) {
 			fix = c - '0';
 			fprintf(stderr, "Selected farm %zu (%p)\n", fix,
 			    farmp[fix]);
