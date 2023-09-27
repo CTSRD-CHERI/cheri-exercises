502ad0cf6b32a68f2313ab1e53ee4f86757f6fb2
George V. Neville-Neil <gnn@neville-neil.com>
2023-09-27 20:50:52 +0100

    Clean up unused flags copied from ccc.

--- src/exercises/common.mk
+++ src/exercises/common.mk
@@ -2,8 +2,8 @@ CC=clang
 
 ARCH!=uname -m
 .if ${ARCH} == "arm64"
-CFLAGS=-g -O2 -target aarch64-unknown-freebsd -mno-relax -Wall -Wcheri
-CFLAGS_CHERI=-march=morello+noa64c -march=morello+c64 -mabi=purecap -Xclang -morello-vararg=new
+CFLAGS=-g -O2 -target aarch64-unknown-freebsd -Wall -Wcheri
+CFLAGS_CHERI=-march=morello+noa64c -march=morello -mabi=purecap -Xclang -morello-vararg=new
 .else
 CFLAGS=-g -O2 -target riscv64-unknown-freebsd -Wall -Wcheri -mno-relax
 CFLAGS_CHERI=-march=rv64gcxcheri -mabi=l64pc128d 
