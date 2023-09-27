a11e6613217307198de87090024fa435c11e3347
George V. Neville-Neil <gnn@neville-neil.com>
2023-09-27 20:42:53 +0100

    Simplify Makefiles using suggestions from Jessica Clarke

--- src/exercises/common.mk
+++ src/exercises/common.mk
@@ -3,12 +3,24 @@ CC=clang
 ARCH!=uname -m
 .if ${ARCH} == "arm64"
 CFLAGS=-g -O2 -target aarch64-unknown-freebsd -mno-relax -Wall -Wcheri
-CFLAGS_CHERIABI=-march=morello+noa64c
-CFLAGS_CAPABI=-march=morello+c64 -mabi=purecap -Xclang -morello-vararg=new
+CFLAGS_CHERI=-march=morello+noa64c -march=morello+c64 -mabi=purecap -Xclang -morello-vararg=new
 .else
-CFLAGS=-g -O2 -target riscv64-unknown-freebsd -mno-relax -Wall -Wcheri
-CFLAGS_CHERIABI=-march=rv64gc -mabi=lp64d 
-CFLAGS_CAPABI=-march=rv64gcxcheri -mabi=l64pc128d
+CFLAGS=-g -O2 -target riscv64-unknown-freebsd -Wall -Wcheri -mno-relax
+CFLAGS_CHERI=-march=rv64gcxcheri -mabi=l64pc128d 
 .endif
+
+.for progbase in ${PROGBASES}
+.for variant in baseline cheri
+CLEANFILES+=	${progbase}-${variant}
+
+all: ${progbase}-${variant}
+
+${progbase}-${variant}: ${progbase}.c
+	${CC} -o ${.TARGET} ${.ALLSRC} ${CFLAGS} ${CFLAGS_${variant:tu}}
+.endfor
+.endfor
+
+clean:
+	rm -f ${CLEANFILES}
 
 .PHONY: all clean
